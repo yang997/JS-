2988b97e618f9fce141fd537266bc5524cd05722
yang997 <837468541@qq.com>
2019-08-18 23:41:12 +0800

    ok bigboby

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ console.log('hello world!');
 // say hello
 console.log(' hei!Siri, I love you 2019')
 
-<<<<<<< HEAD
-console.log('我是yang1分支')
-=======
-
-
-console.log('我是yang2分支')
->>>>>>> yang2
+console.log('I am master')
