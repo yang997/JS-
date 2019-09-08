e19bf43fb31f721b65ffd413c5ceece4e27b6764
yang997 <837468541@qq.com>
2019-09-09 03:05:35 +0800

    我更改了9月9

--- index.js
+++ index.js
@@ -5,14 +5,3 @@ console.log(' hei!Siri, I love you 2019')
 
 
 console.log('我不想一样1')
-
-console.log('I am master2')
-<<<<<<< HEAD
-console.log('我也不知道为什么要拉取')
-
-console.log('像风一下自由')
-=======
-
-console.log('我是一个新的分支')
-
->>>>>>> yang
